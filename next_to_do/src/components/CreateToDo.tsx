f10ba5febee94eab25156e7a37bbbf0edc97cdb1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-19 09:27:26 -0300

    :sparkles: create: component - CreateToDo (@Componenets)

---
+++ next_to_do/src/components/CreateToDo.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+export const CreateToDo = () => {
+
+  const [description, setDescription] = useState("")
+
+  const handleClick = async () => {
+    await fetch("/api/todo", {
+      method: "POST",
+      body: JSON.stringify(description)
+    })
+  }
+
+  return (
+    <form className="flex justify-center mt-10">
+      
+      <div className="bg-gray-50 p-8 rounded-lg">
+        
+        <h1 className="text-center mb-4">
+          Write To Do List
+        </h1>
+
+        <div className="flex space-x-2 p-2 bg-white rounded-md">
+          <input
+            value={description}
+            onChance={event => setDescription(event.currentTarget.value)}
+            type="text"
+            placeholder="Write here..."
+            className="w-full outline-none"
+          />
+          <button
+            className="bg-green-500 px-2 py-1 rounded-md text-white font-semibold"
+            onClick={() => handleClick()}
+          >
+            Send
+          </button>
+        </div>
+
+      </div>
+    
+    </form>
+  )
+}
