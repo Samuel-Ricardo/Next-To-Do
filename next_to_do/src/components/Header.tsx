9d440c4987e3ab28aee06c5152d8e985825b8946
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-19 00:09:00 -0300

    :sparkles: update: component - Header

--- next_to_do/src/components/Header.tsx
+++ next_to_do/src/components/Header.tsx
@@ -1,6 +1,10 @@
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ['latin'] })
+
 export const Header = () => (
   <nav className="flex justify-center p-4 bg-gray-600">
-    <h1 className = "text-white text-2xl font-bold">
+    <h1 className = {`${inter.className} text-white text-2xl font-bold`}>
       Todo App
     </h1>
   </nav>
