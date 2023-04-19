cb532b27a8cdfd2b50c0500f5975e76ad3f125d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-19 09:50:12 -0300

    :card_file_box: operation: create to do (prisma)

--- next_to_do/prisma/db.ts
+++ next_to_do/prisma/db.ts
@@ -2,4 +2,8 @@ import { prisma } from "@lib/prisma";
 
 export const getAllToDos = async () => await prisma.todo.findMany();
 
-
+export async function createToDo(description: string) {
+  await prisma
+          .todo
+          .create({data:{description}})
+}
