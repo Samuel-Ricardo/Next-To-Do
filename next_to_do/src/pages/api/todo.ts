ef86ecfe06e697b10bf79bc5e8d9c84cbf636e3a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-20 09:01:10 -0300

    :sparkles: create: route - PUT /todo (api)

--- next_to_do/src/pages/api/todo.ts
+++ next_to_do/src/pages/api/todo.ts
@@ -1,14 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { createTodo } from '@lib/db';
+import { createTodo, deleteTodo, updateTodo } from '@lib/db';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-): Promise<void> {
+) {
   
   if(req.method === "POST"){
-    await createTodo( JSON.parse(req.body) );
+    await createTodo( JSON.parse(req.body).description );
     return res.status(200).json({message: "Success"})
   }
+
+  if(req.method === "PUT"){
+    await updateTodo( JSON.parse(req.body) );
+    return res.status(200).json({message: "Success"})
+  }
+
+  return res.status(200).json({HI:" :D "})
 }
