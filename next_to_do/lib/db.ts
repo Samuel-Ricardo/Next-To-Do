9f37f85605823f86f6957b022a9bcc817afdf2d0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-20 08:54:35 -0300

    :sparkles: create: fun -  deleteTodo (@lib)

--- next_to_do/lib/db.ts
+++ next_to_do/lib/db.ts
@@ -1,3 +1,4 @@
+import { Todo } from "@/@types";
 import { prisma } from "./prisma";
 
 export const getAllTodos = async () => await prisma.todo.findMany();
@@ -5,5 +6,13 @@ export const getAllTodos = async () => await prisma.todo.findMany();
 export async function createTodo(description: string) {
   await prisma
     .todo
-    .create({ data: description })
+    .create({ data: {description} })
 }
+
+export async function deleteTodo(id:number) {
+  return await prisma
+                .todo
+                .delete({ where:{id} })
+}
+
+
